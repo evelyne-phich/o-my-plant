cd4bed24d0b2f3f3ef20788d944d026f024f8f63
Marie-Gueho <marie.gueho@gmail.com>
2022-01-21 11:58:41 +0100

    test connection front-back localhost OK

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,7 +1,23 @@
+import Axios from "axios";
+import Nav from "../Nav";
 import "./style.scss";
 
 const App = () => {
-  return <div className="app">Hello World</div>;
+  const tst = () => {
+    Axios.get("https://omyplant.herokuapp.com/").then((response) => {
+      console.log(response.data);
+    });
+  };
+
+  return (
+    <div className="app">
+      Hello World
+      <Nav />
+      <button type="button" onClick={tst}>
+        tst
+      </button>
+    </div>
+  );
 };
 
 export default App;
