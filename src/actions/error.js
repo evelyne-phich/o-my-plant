f8394041b5b51ab64b19613a03c73ee19d4f5620
Evelyne PHICH <evelyne.phich@gmail.com>
2022-01-30 14:17:22 +0100

    Removes unused actions

--- src/actions/error.js
+++
@@ -1,7 +0,0 @@
-export const DISPLAY_USER_ALREADY_EXISTS_ERROR_MESSAGE =
-  "DISPLAY_USER_ALREADY_EXISTS_ERROR_MESSAGE";
-
-export const displayUserAlreadyExistsErrorMessage = (payload) => ({
-  type: DISPLAY_USER_ALREADY_EXISTS_ERROR_MESSAGE,
-  payload,
-});
