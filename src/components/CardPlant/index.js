85e67667aff85f7a13c4b40876a547604d6a57f3
Rascazlour <rascalou.raid@gmail.com>
2022-01-31 13:19:23 +0100

    card component

--- src/components/CardPlant/index.js
+++ src/components/CardPlant/index.js
@@ -1,15 +1,19 @@
 import Proptypes from "prop-types";
 import "./style.scss";
 
-const CardPlant = () => (
+const CardPlant = ({ contentButton, img }) => (
   <div className="card">
-    <div className="card-image"></div>
-    <div className="card-desc">
-      <p>titre</p>
-      <p>Description</p>
+    <div className="card-image">
+      <img src={img} alt="photo plant"></img>
     </div>
-    <div className="card-button">
-      <button>Modifier</button>
+    <div className="card-bottom-part">
+      <div className="card-desc">
+        <p className="card-desc-title">titre</p>
+        <p className="card-desc-content">Description</p>
+      </div>
+      <div className="card-button">
+        <button>{contentButton}</button>
+      </div>
     </div>
   </div>
 );
